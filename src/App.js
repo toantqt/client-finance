ffea2c1713b70580d082c1e7c8764e1875acc76c
toantqt <toantqt0609@gmail.com>
2022-02-26 02:45:04 +0700

    update route

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <HashRouter>
       <Route
-        exact
         path="/admin"
         render={() =>
           checkRole("admin") ? <Dashboard /> : <Redirect to="/auth/login" />
