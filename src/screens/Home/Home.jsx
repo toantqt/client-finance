ac068cc7e7b5b6d87b127eb01d5738d0e3923797
toantqt <toantqt0609@gmail.com>
2022-04-14 22:44:20 +0700

    test

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,50 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      arr: [{ name: "toan" }, { name: "nga" }],
-      index: "",
-    };
-  }
-  show = (data) => {
-    return data.map((e, index) => {
-      return (
-        <div>
-          <span
-            onClick={() => {
-              this.handleClick(index);
-            }}
-          >
-            {e.name}
-          </span>
-        </div>
-      );
-    });
-  };
-  handleClick = (index) => {
-    this.setState({
-      index: index,
-    });
-  };
-
-  handleChange = (event) => {
-    let name = event.target.value;
-    console.log(name);
-  };
-  render() {
-    return (
-      <div>
-        {this.show(this.state.arr)}
-        <input
-          type="text"
-          onChange={this.handleChange}
-          defaultValue={this.state.arr[this.state.index]?.name}
-        ></input>
-      </div>
-    );
-  }
+export default function Home() {
+  return (
+    <iframe
+      id="player"
+      type="text/html"
+      width="640"
+      height="390"
+      src="http://www.youtube.com/embed/DnNHOiYeL38"
+      frameborder="0"
+    ></iframe>
+  );
 }
-
-export default Home;
